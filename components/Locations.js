92b88632652b6e99accdfefe5ddb8346b107e944
Ivor Williams <ivor.williams@gmail.com>
2016-07-17 21:42:18 -0400

    Add link to location, extract some functions for mapping, define prop types

--- components/Locations.js
+++ components/Locations.js
@@ -1,29 +1,61 @@
 import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 import uniq from 'lodash/uniq'
+import uniqBy from 'lodash/uniqBy'
 
 class Locations extends React.Component {
+
   static propTypes = {
-    locIDs: React.PropTypes.arrayOf(
-      React.PropTypes.string.isRequired
+    locations: React.PropTypes.arrayOf(
+      React.PropTypes.shape({
+        id: React.PropTypes.string.isRequired,
+        name: React.PropTypes.string.isRequired,
+        lat: React.PropTypes.number.isRequired,
+        lng: React.PropTypes.number.isRequired,
+        hotspot: React.PropTypes.bool.isRequired
+      }).isRequired
     ).isRequired
   }
+
   render() {
     return (
       <div className="locations">
         <ul>
-          {this.props.locIDs.map(locID =>
-            <li key={locID}>{locID}</li>
+          {this.props.locations.map(location =>
+            <li key={location.id}>
+              <a href={this.urlFor(location)}>
+                {location.name} ({location.id})</a>
+                lat={location.lat}, lng={location.lng}
+              )
+            </li>
           )}
         </ul>
       </div>
     )
   }
+
+  urlFor(location) {
+    return location.hotspot ? `http://ebird.org/ebird/hotspot/${location.id}` : '#'
+  }
 }
 
 const mapStateToProps = (state) => {
   return {
-    locIDs: uniq(state.observations.map(x => x.locID))
+    locations: uniqBy(observationsToLocations(state.observations), 'id')
+  }
+}
+
+const observationsToLocations = (observations) => {
+  return observations.map(observationToLocation)
+}
+
+const observationToLocation = (observation) => {
+  return {
+    id: observation.locID,
+    name: observation.locName,
+    lat: observation.lat,
+    lng: observation.lng,
+    hotspot: !observation.locationPrivate
   }
 }
 
