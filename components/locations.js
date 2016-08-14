7fd3ebeb85f5801e64acc6e499e3cd9a00db40f7
Ivor Williams <ivor.williams@gmail.com>
2016-08-14 15:40:12 -0400

    Handle minimum zoom and empty results set

--- components/locations.js
+++ components/locations.js
@@ -46,14 +46,40 @@ class Locations extends React.Component {
   }
 
   zoomMapToMarkers(map) {
-    if (map && !this.props.fetching) {
+    if (map && !this.props.fetching && this.props.markers.length > 0) {
       console.log(`zooming map to ${this.props.markers.length} markers`)
-      let mapBounds = this.props.markers.reduce(
-        // TODO: I should be able to simplify the extend() param
-        (bounds, m) => bounds.extend(new google.maps.LatLng(m.position.lat, m.position.lng)),
-        new google.maps.LatLngBounds()
-      )
+      let mapBounds = this.props.markers
+        .map(m => new google.maps.LatLng(m.position))
+        .reduce(
+          (bounds, ll) => bounds.extend(ll),
+          new google.maps.LatLngBounds()
+        )
+      this.extendToMinimum(mapBounds)
       map.fitBounds(mapBounds)
+    }
+  }
+
+  /*
+   * Without this, er, feature, when we are only showing one marker we will end
+   * up zooming in very close.  If the marker is in the middle of a wetland or
+   * woodland area, then we may be left viewing an entirely blue or green map.
+   * So, this hack ensures that we don't zoom in too close.
+   *
+   * It's a shame that react-google-maps doesn't expose the setZoom() method,
+   * because getting and setting would do the trick.
+   */
+  extendToMinimum(bounds) {
+    const minimumSpan = 0.025 // Arbitrary span, looks about right
+    if (bounds.toSpan().lat() < minimumSpan && bounds.toSpan().lng() < minimumSpan) {
+      let center = bounds.getCenter()
+      bounds.extend(new google.maps.LatLng({
+        lat: center.lat() + minimumSpan,
+        lng: center.lng() + minimumSpan
+      }))
+      bounds.extend(new google.maps.LatLng({
+        lat: center.lat() - minimumSpan,
+        lng: center.lng() - minimumSpan
+      }))
     }
   }
 
